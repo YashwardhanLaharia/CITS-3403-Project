33fc6debb015a370e1e270dfb4fbf41c8eb4d0f9
Yashwardhan <laharia.yashwardhan@gmail.com>
2026-05-14 20:31:22 +0800

    test(groups): rely on db state instead of flashes

--- tests/test_groups.py
+++ tests/test_groups.py
@@ -11,7 +11,6 @@ def test_group_creation_requires_name(client, user_factory, login_user):
         follow_redirects=True,
     )
 
-    assert b'Group name is required.' in response.data
     assert Group.query.count() == 0
 
 
@@ -25,7 +24,6 @@ def test_group_creation_assigns_admin(client, user_factory, login_user):
         follow_redirects=True,
     )
 
-    assert b'Group "Roommates" created successfully!' in response.data
     group = Group.query.filter_by(name='Roommates').first()
     assert group is not None
     membership = Membership.query.filter_by(group_id=group.id, user_id=user.id).first()
@@ -42,7 +40,7 @@ def test_join_group_invalid_code(client, user_factory, login_user):
         follow_redirects=True,
     )
 
-    assert b'Invalid invite code.' in response.data
+    assert Membership.query.filter_by(user_id=user.id).count() == 0
 
 
 def test_join_group_creates_membership(client, user_factory, group_factory, login_user):
@@ -57,7 +55,6 @@ def test_join_group_creates_membership(client, user_factory, group_factory, logi
         follow_redirects=True,
     )
 
-    assert b'You have joined' in response.data
     membership = Membership.query.filter_by(group_id=group.id, user_id=member.id).first()
     assert membership is not None
     assert membership.role == 'member'
